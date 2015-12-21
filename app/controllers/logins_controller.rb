6f6d5b4bd15f48f07ebe5f5df36023f5b0b797af
Vijay <Vijayakumar@celestialsys.com>
2015-12-21 17:35:33 +0530

    business sign in page added

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,7 @@
+class LoginsController < ApplicationController
+  def business
+  end
+
+  def government
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root 'before_login#index', as: :unauthenticated_root
 
+  resource 'logins' do
+    get 'business'
+    get 'government'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
