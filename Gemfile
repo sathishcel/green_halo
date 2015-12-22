40cf1cfc10fcce0a5b4bdefee763b58c1385deb7
sathishcel <tsathish@celestialsys.com>
2015-12-22 20:36:23 +0530

    Added sign-in pages

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'devise'
 gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+
   gem 'mysql2', '~> 0.3.13'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222074335) do
+ActiveRecord::Schema.define(version: 20151222131810) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
